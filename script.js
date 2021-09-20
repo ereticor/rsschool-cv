0be157b608d05dd2b8e44555c94f62885dc1fa5f
ereticor <ereticor@gmail.com>
2021-09-20 19:14:55 +0300

    feat: add header & menu interaction

---
+++ script.js
@@ -0,0 +1,16 @@
+const body = document.body;
+const menu = document.querySelector(".header__menu");
+const nav = document.querySelector(".header__nav");
+const navOver = document.querySelector(".menu_over");
+
+menu.addEventListener("click", () => {
+  nav.classList.toggle("header__nav_active");
+  menu.classList.toggle("active");
+  body.classList.toggle("menu__open");
+});
+
+navOver.addEventListener("click", () => {
+  nav.classList.remove("header__nav_active");
+  menu.classList.remove("active");
+  body.classList.remove("menu__open");
+});
